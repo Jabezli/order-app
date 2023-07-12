8a91c6122c1eb221dde82223ebe6178b30711aff
Jabez <jabezli1990@gmail.com>
2023-07-11 22:43:13 -0700

    image saved

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <p>Hi</p>
+      <button>click me</button>
+    </div>
+  );
+}
+
+export default App;
